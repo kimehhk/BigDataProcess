60a88fef9f87688b66ae870f84a1781ae12cd735
kimehhk <kimehhk@gmail.com>
2022-10-23 16:52:03 +0900

    param seperate

---
+++ HW3/IMDBStudent20190951.py
@@ -0,0 +1,21 @@
+
+a = input()
+b = input()
+dic = {}
+
+with open(a, "rt") as f:
+	for line in f:
+		arr = line.split('::')
+		genre = arr[2].split('|')
+		for g in genre:
+			g = g.strip()
+			if g in dic:
+				dic[g] += 1
+			else:
+				dic[g] = 1
+
+with open(b, "wt") as f:
+	keylist = dic.keys()
+	for key in keylist:
+		s = key + " " + str(dic[key]) + "\n"
+		f.write(s)
