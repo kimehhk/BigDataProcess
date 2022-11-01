685d50e87796711eadce04ede2383d2aff9e88ee
kimehhk <kimehhk@gmail.com>
2022-11-02 02:39:13 +0900

    uber

---
+++ HW3/UBERStudent20190951.py
@@ -0,0 +1,20 @@
+import datetime as dt
+
+a, b = input().split()
+
+def day_is(m, d, y):
+	days = ['MON','TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+	day = dt.date(int(y), int(m), int(d)).weekday()
+	return days[day]
+
+with open(a, "rt") as a, open(b, "wt") as b:
+	for line in a:
+		arr = line.split(',')
+		region = arr[0]
+		d = arr[1].split('/')
+		day = day_is(d[0], d[1], d[2])
+		vehicles = arr[2]
+		trips = arr[3]	
+		s = region + "," + day + " " + vehicles + "," + trips
+		b.write(s)
+
